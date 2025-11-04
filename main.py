412cf8f38a5907de7fc9dc5c341e8217ca9565d2
minh95702 <minh95702@st.vimaru.edu.vn>
2025-11-04 18:22:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -30,4 +30,6 @@ def get_command():
     return cmd
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render sẽ tự thay PORT khi deploy
+    app.run(host="0.0.0.0", port=port)
